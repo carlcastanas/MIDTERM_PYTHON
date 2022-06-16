996fca80fb308da4709d09aa3939a9485c918f3c
Carl Andrew Castañas <89068725+carlcastanas@users.noreply.github.com>
2022-06-16 22:15:03 +0800

    Create multiply.py

---
+++ multiply.py
@@ -0,0 +1,4 @@
+# 2. Program using 2 list, where list A will multiply itself and transfer its value to list B.
+distance = float(input("Enter value in distance: "))
+km = distance * 1000
+print('%0.2f distance is distance to %0.2f kilometers' %(distance,km))
