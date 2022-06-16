6a25a8066a524d49b0020ca89436d9ceb3fbb69c
Carl Andrew Castañas <89068725+carlcastanas@users.noreply.github.com>
2022-06-16 22:16:09 +0800

    Update and rename multiply.py to distance.py

--- distance.py
+++ distance.py
@@ -1,4 +1,5 @@
-# 2. Program using 2 list, where list A will multiply itself and transfer its value to list B.
+# 1.	Program that accepts a given distance and convert it to its corresponding KM value using the formula: km = distance*1000  
+
 distance = float(input("Enter value in distance: "))
 km = distance * 1000
 print('%0.2f distance is distance to %0.2f kilometers' %(distance,km))
